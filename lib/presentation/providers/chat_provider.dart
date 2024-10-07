a08cad41153531d95ba2629a373e1a2eefdb5daa
valllllll2000 <valerie.cardoso@gmail.com>
2024-10-07 17:26:02 +0200

    initial app code

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
+
+import '../../domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
+
+  List<Message> messageList = [
+    Message(text: 'Hola amor!', fromWho: FromWho.me),
+    Message(text: 'ya regresaste del trabajo?', fromWho: FromWho.me),
+  ];
+
+  Future<void> theirReply() async {
+    final theirMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(theirMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+    if (text.endsWith('?')) {
+      theirReply();
+    }
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeOut);
+  }
+}
---
+++ lib/presentation/widgets/chat/their_message_bubble.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class TheirMessageBubble extends StatelessWidget {
+  final Message message;
+  const TheirMessageBubble({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              message.text,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        _ImageBubble(message.imageUrl!),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  const _ImageBubble(this.url);
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          url,
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('Mi amor is sending an image'),
+            );
+          },
+        ));
+  }
+}
